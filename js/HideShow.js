5db234a6e634c4e9fad08e024a004f3b48448359
Brian Anders <anders.brian@icloud.com>
2013-11-30 12:54:13 -0800

    slight cleanup

--- js/HideShow.js
+++ js/HideShow.js
@@ -5,14 +5,7 @@
         var defaults = {
             speed: 1000,
             easing: '',
-            changeText: 0,
-            showText: 'Show/Hide',
-            hideText: 'Hide', // the button text to show when a div is open
-		showSign: "+",
-		hideSign: "-",
-		showImg: "img/Show.png",
-		hideImg: "img/Hide.png"
- 
+            changeText: 0
         };
         var options = $.extend(defaults, options);
  
@@ -23,7 +16,7 @@
              var toggleClick = $(this);
              // this reads the rel attribute of the button to determine which div id to toggle
              //var toggleDiv = $(this).attr('rel');
-			 var toggleDiv = $(this).children('p');
+			 var toggleDiv = $(this).siblings('p');
              // here we toggle show/hide the correct div at the right speed and using which easing effect
              $(toggleDiv).slideToggle(options.speed, options.easing, function() {
              // this only fires once the animation is completed
@@ -43,17 +36,9 @@
 
 
 $(document).ready(function(){
- 
    $('.show_hide').showHide({
         speed: 400,  // speed you want the toggle to happen
         easing: '',  // the animation effect you want. Remove this line if you dont want an effect and if you haven't included jQuery UI
-        changeText: 1, // if you dont want the button text to change, set this to 0
-        showText: 'Show',// the button text to show when a div is closed
-        hideText: 'Hide', // the button text to show when a div is open
-		showSign: "+",
-		hideSign: "--",
-		showImg: "img/Show.png",
-		hideImg: "img/Hide.png"
+        changeText: 1 // if you dont want the button text to change, set this to 0
     });
- 
 });
